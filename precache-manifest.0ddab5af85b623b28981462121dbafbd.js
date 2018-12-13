fc09918ee4864713741476a2d60ac37a7afd994d
pj13111995 <praveen.poonja@novigosolutions.com>
2018-12-13 12:57:18 +0530

    Updates

---
+++ precache-manifest.0ddab5af85b623b28981462121dbafbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "69c39e8737028331e464",
+    "url": "/react-gh-pages/static/js/main.69c39e87.chunk.js"
+  },
+  {
+    "revision": "d30bc6dbb8eaf399fc85",
+    "url": "/react-gh-pages/static/js/1.d30bc6db.chunk.js"
+  },
+  {
+    "revision": "69c39e8737028331e464",
+    "url": "/react-gh-pages/static/css/main.6499c741.chunk.css"
+  },
+  {
+    "revision": "d30bc6dbb8eaf399fc85",
+    "url": "/react-gh-pages/static/css/1.f662e6c9.chunk.css"
+  },
+  {
+    "revision": "3d112330f548779cdf21915e8b3490d0",
+    "url": "/react-gh-pages/index.html"
+  }
+];
